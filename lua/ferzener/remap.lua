667043fa8f5f2f400e8656b0b8aed7f51e6c4de6
Pedro Renan Mussato <81251621+PedroMussato@users.noreply.github.com>
2024-06-18 09:30:48 -0300

    Update remap.lua

--- lua/ferzener/remap.lua
+++ lua/ferzener/remap.lua
@@ -1,6 +1,22 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+
+-- Define a function to prompt for a filename and create a new file
+function create_new_file()
+    local filename = vim.fn.input('Enter new file name: ')
+    if filename ~= '' then
+        vim.cmd('edit ' .. filename)
+    else
+        print("No filename provided")
+    end
+end
+
+-- Map % to call the custom function
+vim.api.nvim_set_keymap('n', '<leader>n', ':lua create_new_file()<CR>', { noremap = true, silent = true })
+
+
+
 vim.keymap.set("n", "<leader>so", vim.cmd.so)
 
 vim.keymap.set("n", "<C-k>", "<C-d>zz")
@@ -20,4 +36,3 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<left><left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
-
